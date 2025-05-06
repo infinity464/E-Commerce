5ea3939b2bbafefcc7b8ce07f22550972ac2225f
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-05-06 16:41:10 +0600

    navigation bar re design

---
+++ asset/JavaScript/app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const overlay = document.querySelector(".overlay");
+    const sidebarCollapse = document.getElementById("sidebarCollapse");
+    const sidebarCollapseX = document.getElementById("sidebarCollapseX");
+
+    sidebarCollapse.addEventListener("click", function () {
+      sidebar.classList.add("active");
+      overlay.classList.add("visible");
+    });
+
+    sidebarCollapseX.addEventListener("click", function () {
+      sidebar.classList.remove("active");
+      overlay.classList.remove("visible");
+    });
+
+    overlay.addEventListener("click", function () {
+      sidebar.classList.remove("active");
+      overlay.classList.remove("visible");
+    });
+  });
