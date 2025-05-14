7dd02aade8b828810e4baa363765a2d4085415d7
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-05-14 12:56:05 +0600

    navigation revaped

--- asset/JavaScript/app.js
+++ asset/JavaScript/app.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.getElementById("sidebar");
     const overlay = document.querySelector(".overlay");
     const sidebarCollapse = document.getElementById("sidebarCollapse");
-    const sidebarCollapseX = document.getElementById("sidebarCollapseX");
+    const 
+    sidebarCollapseX = document.getElementById("sidebarCollapseX");
 
     sidebarCollapse.addEventListener("click", function () {
       sidebar.classList.add("active");
